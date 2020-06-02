2556c815a56e00e380d8d6d78ba2c9d08fd2eccb
Johnny Barrantes <jbarrantes@gmail.com>
2020-06-02 17:08:37 -0600

    create models and dbcontext

---
+++ SPASolution/src/Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence.Database
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+
+    }
+}
